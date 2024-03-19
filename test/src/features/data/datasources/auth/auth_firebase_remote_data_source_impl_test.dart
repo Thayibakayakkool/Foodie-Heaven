3cceeed64527b2174c98ef52a6cc9449c1457e21
Thayibakayakkool <thayibakayakool@gmail.com>
2024-03-19 12:23:03 +0530

    commit

--- test/src/features/data/datasources/auth/auth_firebase_remote_data_source_impl_test.dart
+++ test/src/features/data/datasources/auth/auth_firebase_remote_data_source_impl_test.dart
@@ -25,12 +25,15 @@ void main() {
 
   group('AuthFirebaseRemoteDataSourceImpl class-', () {
     test('should call sendPasswordResetEmail with correct email', () async {
+      // Arrange
       final String email = 'test@example.com';
 
       when(() => mockAuth.sendPasswordResetEmail(email: any(named: 'email')))
           .thenAnswer((_) => Future.value());
+      // Act
       await authFirebase.forgotPassword(email);
 
+     // Assert
       verify(() => mockAuth.sendPasswordResetEmail(email: email)).called(1);
     });
   });
@@ -48,56 +51,61 @@ void main() {
 
   group('isLogIn', () {
     test('isLogIn returns true when there is a user', () async {
-
+      // Arrange
       when(() => mockUser.uid).thenReturn('testUid');
       when(() => mockAuth.currentUser).thenReturn(mockUser);
-
+      // Act
       final result = await authFirebase.isLogIn();
-
+    // Assert
       expect(result, true);
     });
 
     test('isLogIn returns false when there is no use', () async {
+      // Arrange
       when(() => mockAuth.currentUser).thenReturn(null);
+      // Act
       final result = await authFirebase.isLogIn();
-
+     // Assert
       expect(result, false);
     });
   });
 
   test('Successful login', () async {
+    // Arrange
     final userEntity =
-    UserEntity(email: 'test@example.com', password: 'password123');
+        UserEntity(email: 'test@example.com', password: 'password123');
 
     when(() => mockAuth.signInWithEmailAndPassword(
-        email: userEntity.email!, password: userEntity.password!))
+            email: userEntity.email!, password: userEntity.password!))
         .thenAnswer((_) async => mockUserCredential);
-
+    // Act
     await authFirebase.logIn(userEntity);
-
+   // Assert
     verify(() => mockAuth.signInWithEmailAndPassword(
         email: userEntity.email!, password: userEntity.password!)).called(1);
   });
 
   test('logOut', () async {
+    // Arrange
     when(() => mockAuth.signOut()).thenAnswer((_) => Future.value());
+    // Act
     await authFirebase.logOut();
+    // Assert
     verify(() => mockAuth.signOut()).called(1);
   });
 
   test('successfully signs up the user', () async {
+    // Arrange
     final userEntity =
-    UserEntity(email: 'test@example.com', password: 'password123');
+        UserEntity(email: 'test@example.com', password: 'password123');
 
     when(() => mockAuth.createUserWithEmailAndPassword(
-        email: userEntity.email!, password: userEntity.password!))
+            email: userEntity.email!, password: userEntity.password!))
         .thenAnswer((_) async => mockUserCredential);
-
+    // Act
     await authFirebase.signUp(userEntity);
-
+   // Assert
     verify(() => mockAuth.createUserWithEmailAndPassword(
         email: userEntity.email!, password: userEntity.password!)).called(1);
   });
-
 }
-
